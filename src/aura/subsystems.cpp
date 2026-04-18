9ce1d480f05a353b6b36a1fc6464127c2d29fd9e
goofyjnr <goofyjnr@gmail.com>
2026-04-18 12:11:44 +1200

    start of auton

--- src/aura/subsystems.cpp
+++ src/aura/subsystems.cpp
@@ -34,7 +34,7 @@ namespace subsystems {
                             pros::v5::MotorGearset::red,
                             pros::v5::MotorEncoderUnits::degrees)),
         lever_angle(pros::adi::Pneumatics(lever_angle_port, false)),
-        hood(pros::adi::Pneumatics(hood_port, false)),
+        hood(pros::adi::Pneumatics(hood_port, true)),
         // kP, kI, kD, start_i
         lever_pid(10.0, 0.5, 9.0, 0.0, "Lever PID")
     {    
@@ -121,7 +121,7 @@ namespace subsystems {
                 currentMode == LEVER_SLOW   ||
                 currentMode == LEVER_MANUAL
             );
-            hood.set_value(hoodOpen);
+            hood.set_value(!hoodOpen);
 
 
             //set the state for the lever
@@ -268,7 +268,7 @@ namespace subsystems {
         //move the lever to target position at set speed and then waint at the top
         void lever::autoScore(double target_position, int speed, int wait_time_ms) {
             //open hood
-            hood.set_value(true);
+            hood.set_value(false);
 
             //set the PID target and spin speed
             setLeverTarget(target_position, speed);
@@ -280,7 +280,7 @@ namespace subsystems {
             pros::delay(wait_time_ms);
 
             //close the hood
-            hood.set_value(false);
+            hood.set_value(true);
 
             //bring the lever back down safely to the hard stop
             autoHome();
@@ -488,7 +488,7 @@ namespace subsystems {
             //hold L1 to extend
             //release to retract
             bool buttonHeld = master.get_digital(pros::E_CONTROLLER_DIGITAL_R1);
-            setState(!buttonHeld);
+            setState(buttonHeld);
         }
        
 
--- src/autons.cpp
+++ src/autons.cpp
@@ -18,9 +18,9 @@ const int SWING_SPEED = 110;
 ///
 void default_constants() {
   // P, I, D, and Start I
-  chassis.pid_drive_constants_set(8.0, 0.0, 8.0);         // Fwd/rev constants, used for odom and non odom motions
+  chassis.pid_drive_constants_set(6.5, 0.005, 10);         // Fwd/rev constants, used for odom and non odom motions
   chassis.pid_heading_constants_set(11.0, 0.0, 20.0);        // Holds the robot straight while going forward without odom
-  chassis.pid_turn_constants_set(3.0, 0.05, 20.0, 15.0);     // Turn in place constants
+  chassis.pid_turn_constants_set(4.0, 0.04, 25.0, 15.0);     // Turn in place constants
   chassis.pid_swing_constants_set(6.0, 0.0, 65.0);           // Swing constants
   chassis.pid_odom_angular_constants_set(6.5, 0.0, 52.5);    // Angular control for odom motions
   chassis.pid_odom_boomerang_constants_set(5.8, 0.0, 32.5);  // Angular control for boomerang motions
@@ -386,7 +386,8 @@ void leftSideAuton(){
   chassis.odom_xyt_set(-47, -5, 0);
 
   //move to center point
-  chassis.pid_odom_set({{-47, -47}, fwd, 70});
+  // chassis.pid_odom_set({{-47, -47}, fwd, 70});
+  chassis.pid_drive_set(40.3_in, 110);
   chassis.pid_wait();
 
   //turn to match loader
@@ -396,19 +397,30 @@ void leftSideAuton(){
 
   //put down match loader
   matchload.setState(true);
+  pros::delay(700);
 
-  //drive to match loader
-  chassis.pid_drive_set(10, 60);
+  // //drive to match loader
+  // chassis.pid_drive_set(10, 60);
+  chassis.pid_drive_set(11.5_in, 60);
+
   chassis.pid_wait();
 
-  //match load grabing 3 balls
+  // //match load grabing 3 balls
+  lever.autoAngleShift(subsystems::LEVER_UP);
   intake.autoIndex();
-  pros::delay(2000);
+  // chassis.pid_drive_set(0.2_in, 10);
+  // chassis.pid_wait();
+  pros::delay(1400);
   intake.stopAuto();
+  pros::delay(100);
 
   //drive to score
+  chassis.pid_drive_set(-29_in, 60);
+  chassis.pid_wait();
 
   //score
+  lever.autoScore(130.0, 90,  800);
+  pros::delay(1000);
 
   //drive back to match loader
     //clear out opposite blocks
@@ -424,10 +436,10 @@ void leftSideAuton(){
 
   
 
-  lever.autoScore(130.0, 127,  1000);
+  // lever.autoScore(130.0, 127,  1000);
 
-  // Example: Score SLOW in the LONG goal (LEVER_DOWN), wait 800ms, then come down
-  lever.autoScore(100.0, 65,  800);
+  // // Example: Score SLOW in the LONG goal (LEVER_DOWN), wait 800ms, then come down
+  // lever.autoScore(100.0, 65,  800);
   
 }
 
